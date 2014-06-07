8dcc08437ae9706b26b53c0358661b5df068a37a
mldeng <wndmlang@gmail.com>
2014-06-07 22:46:44 +0800

    add base class

--- library/src/com/damon/ds/app/DSActivity.java
+++ library/src/com/damon/ds/app/DSActivity.java
@@ -2,10 +2,12 @@ package com.damon.ds.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
 public class DSActivity extends FragmentActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
@@ -13,33 +15,70 @@ public class DSActivity extends FragmentActivity {
 		onSetContent();
 		DSApplication.instance().activityOnCreate(this);
 	}
-	
-	protected void onHandleIntent(){
-		
+
+	protected void onHandleIntent() {
+
 	}
-	
-	protected void onSetContent(){
-		
+
+	protected void onSetContent() {
+
 	}
-	
+
+	public void addFramgent(Class<?> clss, Bundle args) {
+		addFramgent(android.R.id.content, null, clss, args);
+	}
+
+	public void addFramgent(String tag, Class<?> clss, Bundle args) {
+		addFramgent(android.R.id.content, tag, clss, args);
+	}
+
+	public void addFramgent(int containerViewId, String tag, Class<?> clss, Bundle args) {
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		Fragment fragment = Fragment.instantiate(this, clss.getName(), args);
+		transaction.add(containerViewId, fragment, tag);
+		transaction.addToBackStack(null);
+		transaction.commitAllowingStateLoss();
+		getSupportFragmentManager().executePendingTransactions();
+	}
+
+	public boolean popFragment() {
+		return popFragment(android.R.id.content);
+	}
+
+	public boolean popFragment(int containerViewId) {
+		Fragment fragment = getSupportFragmentManager().findFragmentById(containerViewId);
+		if (fragment != null) {
+			return getSupportFragmentManager().popBackStackImmediate();
+		}
+		return false;
+	}
+
+	public boolean popFragment(String tag) {
+		Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
+		if (fragment != null) {
+			return getSupportFragmentManager().popBackStackImmediate();
+		}
+		return false;
+	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		DSApplication.instance().activityOnResume(this);
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
 		DSApplication.instance().activityOnPause(this);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		DSApplication.instance().activityOnDestory(this);
 	}
-	
+
 	@Override
 	public void startActivityForResult(Intent intent, int requestCode) {
 		super.startActivityForResult(intent, requestCode);
--- library/src/com/damon/ds/app/DSFragment.java
+++ library/src/com/damon/ds/app/DSFragment.java
@@ -2,18 +2,32 @@ package com.damon.ds.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DSFragment extends Fragment {
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		if(!(activity instanceof DSActivity)){
+		if (!(activity instanceof DSActivity)) {
 			throw new IllegalArgumentException("DSFragment must attach to DSActivity!");
 		}
 	}
-	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return onSetView(inflater, container);
+	}
+
+	protected View onSetView(LayoutInflater inflater, ViewGroup container) {
+		return inflater.inflate(android.R.layout.simple_list_item_1, container, false);
+	}
+
 	@Override
 	public void startActivityForResult(Intent intent, int requestCode) {
 		super.startActivityForResult(intent, requestCode);
