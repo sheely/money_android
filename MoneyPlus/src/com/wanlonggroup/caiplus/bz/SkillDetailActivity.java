11ab8f24aa9c604c1ba41df390e1badab7230c5e
mldeng <wndmlang@gmail.com>
2014-09-04 23:26:27 +0800

    fix bug

--- MoneyPlus/src/com/wanlonggroup/caiplus/bz/SkillDetailActivity.java
+++ MoneyPlus/src/com/wanlonggroup/caiplus/bz/SkillDetailActivity.java
@@ -12,54 +12,54 @@ import com.xdamon.app.DSObject;
 import com.xdamon.util.DSObjectFactory;
 
 public class SkillDetailActivity extends BaseActivity {
-	
-	WebView webView;
-	int itemType;
-	String itemId;
-	String itemTitle;
-	DSObject dsSkillDetail;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.skill_detail);
-		
-		webView = (WebView) findViewById(R.id.webview);
-		webView.getBackground().setAlpha(0);
-		
-		itemType = getIntParam("itemtype");
-		itemId = getStringParam("itemid");
-		itemTitle = getStringParam("itemtitle");
-		if(!TextUtils.isEmpty(itemTitle)){
-			setTitle(itemTitle);
-		}
-		
-		updateView();
-		queryDetail();
-	}
-	
-	void updateView(){
-		if(dsSkillDetail == null){
-			return;
-		}
-		webView.loadData(dsSkillDetail.getString("itemContent"), "text/html", "utf-8");
-	}
-	
-	SHPostTaskM detailTask;
-	
-	void queryDetail(){
-		detailTask = getTask(DEFAULT_API_URL+"miQuerySkillCaseDetail.do", this);
-		detailTask.getTaskArgs().put("itemType", itemType);
-		detailTask.getTaskArgs().put("itemId", itemId);
-		detailTask.start();
-		showProgressDialog();
-	}
-	
-	@Override
-	public void onTaskFinished(SHTask task) throws Exception {
-		dismissProgressDialog();
-		dsSkillDetail = DSObjectFactory.create("SkillDetail").fromJson(task.getResult());
-		updateView();
-	}
+    WebView webView;
+    int itemType;
+    String itemId;
+    String itemTitle;
+    DSObject dsSkillDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skill_detail);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        itemType = getIntParam("itemtype");
+        itemId = getStringParam("itemid");
+        itemTitle = getStringParam("itemtitle");
+        if (!TextUtils.isEmpty(itemTitle)) {
+            setTitle(itemTitle);
+        }
+
+        updateView();
+        queryDetail();
+    }
+
+    void updateView() {
+        if (dsSkillDetail == null) {
+            return;
+        }
+        webView.loadDataWithBaseURL(null, dsSkillDetail.getString("itemContent"), "text/html",
+            "utf-8", null);
+    }
+
+    SHPostTaskM detailTask;
+
+    void queryDetail() {
+        detailTask = getTask(DEFAULT_API_URL + "miQuerySkillCaseDetail.do", this);
+        detailTask.getTaskArgs().put("itemType", itemType);
+        detailTask.getTaskArgs().put("itemId", itemId);
+        detailTask.start();
+        showProgressDialog();
+    }
+
+    @Override
+    public void onTaskFinished(SHTask task) throws Exception {
+        dismissProgressDialog();
+        dsSkillDetail = DSObjectFactory.create("SkillDetail").fromJson(task.getResult());
+        updateView();
+    }
 
 }
